0f2b7740722dfbe79f1f75d5f88181b5e9bca83e
vanne02135 <antti.vanne@iki.fi>
2014-08-14 23:29:37 +0300

    Plot RR graph along the other results

--- suunto_hrv.py
+++ suunto_hrv.py
@@ -1,65 +1,54 @@
 #!/usr/bin/python
-# read Suunto XML and determine some HRV values
+# -*- coding: UTF-8 -*-
 
-import sys
+import cgitb
+import cgi
+from suunto_hrv import hrv, read_ibi, filter_rr
 import numpy
-import math
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
+import pylab
 
-def read_ibi_from_xml(filename):
-        return read_ibi(open(filename).read())
-
-def read_ibi(xmldata):
-        # parse IBI values from the Suunto Ambit created XML
-
-        ibi_s = xmldata.find("<IBI>")
-        ibi_e = xmldata.find("</IBI>")
-
-        rr_raw = xmldata[ibi_s+5:ibi_e].split(" ")
-        rr_raw = numpy.array([int(r) for r in rr_raw])
-        return rr_raw
+cgitb.enable()
 
 
-def filter_rr (rr):
-        # to be improved
+class Form:
+	def __init__(self):
+		self.form = """<html><tITLE>Suunto HRV Analysis</title><body>
+Upload a Suunto Ambit XML file to determine your HRV. You should record 1-5 minutes of heart rate after a good rest and with controlled breathing. Copy the HRV values to a spreadsheet and adjust your training according to the changes. Read more from e.g. <a href="http://hrvtraining.com">hrvtraining.com</a><br>
+<form enctype="multipart/form-data" action="mypy.cgi" method="post">
+<p>File: <input type="file" name="file"></p>
+<p><input type="submit" value="Upload"></p>
+</form>
+</body></html>"""
 
-        min_hr = 30
-        max_hr = 200
+response = """<html><title>Suunto HRV analysis</title><body>
+	Results: <br>
+	<table>
+<tr><td>Filename</td><td>HRV (RMSSD)</td><td>HRV (ithlete)</td><td>RR data</td></tr>
+<tr><td>%s</td><td>%f</td><td>%f</td><td><img src="%s"></td></tr>
+</table>	
+		</body></html>"""
 
-        max_rri = 1.0 / (min_hr / 60.0) * 1000
-        min_rri = 1.0 / (max_hr / 60.0) * 1000
+header = """Content-Type: text/html;charset=utf-8
 
-        rr_filtered = []
-        for r in rr:
-                if r >= min_rri and r <= max_rri:
-                        rr_filtered.append(r)
-
-        return numpy.array(rr_filtered)
-
-
-def hrv_rmssd(rr):
-        # Calculate Heart rate variability (RMSSD)
-        sd = numpy.diff(rr)
-
-        sq = [d**2 for d in sd]
-
-        return (numpy.mean(sq))**(0.5)
-
-def hrv_ithlete(rmssd):
-        # Scale the heart rate variability like the application ithlete does
-        # according to http://hrvtraining.com/2013/07/04/rmssd-the-hrv-value-provided-by-ithlete-and-bioforce/
-        return 20 * math.log(rmssd)
-
-def hrv(xmldata):
-        rr_raw = read_ibi(xmldata)
-        rr = filter_rr(rr_raw)
-        hrvrmssd = hrv_rmssd(rr)
-        hrvithlete = hrv_ithlete(hrvrmssd)
-        return (hrvrmssd, hrvithlete)
+"""
 
 if __name__ == "__main__":
-        rr_raw = read_ibi_from_xml(sys.argv[1])
-        rr = filter_rr(rr_raw)
-
-        print "HRV (RMSSD) = %f" % hrv_rmssd(rr)
-        print "HRV (ithlete) = %f" % hrv_ithlete(hrv_rmssd(rr))
+	print header
+	form = cgi.FieldStorage()
+	try:
+		fileitem = form["file"]
+		xmldata = fileitem.file.read()
+		(hrv_rmssd, hrv_ithlete) = hrv(xmldata)
+		rr = filter_rr(read_ibi(xmldata))
+		t = numpy.linspace(0, rr.sum(), len(rr))
+		pylab.plot(t, rr)
+		img_name = "rr.png"
+		pylab.savefig(img_name)
+		print response % (fileitem.filename, hrv_rmssd, hrv_ithlete, img_name)
+	except:
+		f = Form()
+		print f.form
 
